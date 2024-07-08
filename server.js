eee1ad320a634150abb250318376f64cfc6ee88e
root <root@ip-172-31-41-133.ap-south-1.compute.internal>
2024-07-08 11:59:34 +0000

    checking automatic pipeline building

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('welecom welcome wleocmee');});
+  res.send('v2 checking');});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
